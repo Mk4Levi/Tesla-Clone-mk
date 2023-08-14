68128ce3d3f14c57a014bbdf89b8a9aec691febd
Amrit <manthansoul@gmail.com>
2023-08-14 17:26:12 +0530

    created Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Home />
+      <Footer/>
     </div>
   );
 }
