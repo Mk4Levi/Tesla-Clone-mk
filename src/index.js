3d5149982683272b4c551b52c4911bc5f5180384
Amrit <manthansoul@gmail.com>
2023-08-15 08:17:51 +0530

    styled the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./css/index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
